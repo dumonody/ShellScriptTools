8d9158a7af27b36d8f95afbba46bcb65d26ddfa0
dumonody <dumonody@163.com>
2018-02-22 02:46:39 +0800

    Update mysql_configure.sh

--- mysql_configure.sh
+++ mysql_configure.sh
@@ -10,11 +10,20 @@ sqlPasswd=${sqlPasswd##*root@localhost: }
 
 #	经过下面两步设置，密码就可以设置得很简单
 #	设置验证密码方针，将默认的ON设置为LOW
+sleep 250ms
 echo "set global validate_password_policy=0;" > sql.log
+sleep 250ms
+
 #	设置验证密码长度，将默认的8设置为1，这里有个特定算法
+
+sleep 250ms
 echo "set global validate_password_length=4;" >> sql.log
+sleep 250ms
 #	设置新的初始密码，否则不能做任何事情，因为MySQL默认必须修改密码之后才能操作数据库
+
+sleep 250ms
 echo -e "请设置mysql数据库初始密码:\c"
+sleep 250ms
 read
 initPassword=$REPLY
 #echo "您输入的密码是：$initPassword"
@@ -23,10 +32,16 @@ mkdir -p /root/secret/
 touch /root/secret/mysql_initPassword
 
 #	保存到mysql_initPassword中
+
+sleep 250ms
 echo "$initPassword" > /root/secret/mysql_initPassword
+sleep 250ms
 
 #	修改新的初始密码
+
+sleep 250ms
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$initPassword';" >> sql.log
+sleep 250ms
 #	可以通过命令SHOW VARIABLES LIKE 'validate_password%';进行查看
 #echo "SHOW VARIABLES LIKE 'validate_password%';" >> sql.log
 #	将sql文件内容导入，完成初始密码的修改，初始密码保存于mysql_password中
@@ -39,14 +54,24 @@ mysql -uroot -p$sqlPasswd --connect-expired-password < sql.log
 systemctl restart mysqld
 
 #	设置允许远程登录，并具有所有库任何操作权限
+
+sleep 250ms
 echo "set global validate_password_policy=0;" > sql.log
+sleep 250ms
+
+sleep 250ms
 echo "set global validate_password_length=4;" >> sql.log
-sleep 1s
+sleep 250ms
 #	将授权操作语句写入到sql.log文件中
+
+sleep 250ms
 echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$initPassword' WITH GRANT OPTION;" >> sql.log
-sleep 1s
+sleep 250ms
 #	重载授权表
+
+sleep 250ms
 echo "FLUSH PRIVILEGES;" >> sql.log
+sleep 250ms
 
 mysql -uroot -p$initPassword --connect-expired-password < sql.log
 
