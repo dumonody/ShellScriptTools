235c6239f85642b8d714d69d99c1867777b871d9
dumonody <dumonody@163.com>
2018-02-22 11:32:59 +0800

    mysql一键安装脚本

--- mysql.sh
+++ mysql.sh
@@ -2,6 +2,6 @@
 # mysql.sh
 wget https://raw.githubusercontent.com/dumonody/ShellScriptTools/master/mysql_configure.sh
 wget https://raw.githubusercontent.com/dumonody/ShellScriptTools/master/mysql_download.sh
-chmod 777 *
+chmod 755 mysql_configure.sh mysql_download.sh
 ./mysql_download.sh
 ./mysql_configure.sh
